8a67cba55a206d4390f869bc251e6269bb00079c
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2020-04-29 22:46:58 +0500

    worked on slider styles

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.main_slider').slick({
+        infinite: true,
+        dots: true,
+        speed: 300,
+        prevArrow: '<button type="button" class="slick-prev">U+2B9C</button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>'
+    });
+
+});
+
+// <img src="icons/arrow_left.svg">
