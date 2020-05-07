26c346f847d9bfe12303f3caaa6cdbf11eb8412d
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2020-05-07 23:47:15 +0500

    add title instead of dots

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('.main_slider').slick({
+    $('.main__slider').slick({
         infinite: true,
         dots: true,
         speed: 300,
@@ -8,8 +8,19 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
         customPaging : function(slider, i) {
             var thumb = $(slider.$slides[i]).data();
-            return '<div class="main_slider__paging"></div>';
+            return '<div class="main__paging"></div>';
             },
     });
 
+    $('.solutions__slider').slick({
+        infinite: true,
+        dots: true,
+        speed: 300,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/solutions_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/solutions_right.svg"></button>',
+        customPaging: function(slider, i) {
+            var tittle = $(slider.$slides[i]).data('slidertitle');
+            return '<a>' + tittle + '</a>';
+          }
+    });
 });
